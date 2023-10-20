efcfec3f9f2b1f8b50700e405b0fcba681f7c033
Graciel Covanzi <gracovanzi@gmail.com>
2023-10-19 21:37:20 -0300

    tests cases cart

--- marketplace_sitio/cart/tests.py
+++ marketplace_sitio/cart/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Product
+from .cart import Cart
 
-# Create your tests here.
+
+class CartTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass')
+        self.product = Product.objects.create(
+            name='Test Product', price=10.0, image='test.jpg')
+
+    def test_update_cart_increment(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(reverse('update_cart', args=[self.product.id, 'increment']))
+        self.assertEqual(response.status_code, 200)
+        cart = Cart(self.client.session)
+        self.assertEqual(cart.get_item(self.product.id)['quantity'], 1)
+
+    def test_update_cart_decrement(self):
+        self.client.login(username='testuser', password='testpass')
+        cart = Cart(self.client.session)
+        cart.add(product=self.product, quantity=2)
+        response = self.client.get(reverse('update_cart', args=[self.product.id, 'decrement']))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(cart.get_item(self.product.id)['quantity'], 1)
+
+    def test_update_cart_remove(self):
+        self.client.login(username='testuser', password='testpass')
+        cart = Cart(self.client.session)
+        cart.add(product=self.product, quantity=1)
+        response = self.client.get(reverse('update_cart', args=[self.product.id, 'remove']))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(cart.get_item(self.product.id))
+
+    def test_update_cart_clear(self):
+        self.client.login(username='testuser', password='testpass')
+        cart = Cart(self.client.session)
+        cart.add(product=self.product, quantity=1)
+        response = self.client.get(reverse('update_cart', args=[self.product.id, 'clear']))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(cart.get_item(self.product.id))
