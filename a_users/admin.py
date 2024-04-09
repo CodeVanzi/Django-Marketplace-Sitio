a4437a0248cb7a3be0b9cd5981b86c1204727441
Graciel Covanzi <41657320+CodeVanzi@users.noreply.github.com>
2024-04-09 16:23:45 -0300

    Update admin.py

--- a_users/admin.py
+++ a_users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Profile
+from .models import CustomProfile
 
-admin.site.register(Profile)
+admin.site.register(CustomProfile)
 
