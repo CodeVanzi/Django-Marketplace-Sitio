3fe3fc028ec64a4aa35d131853e186c96eb126aa
Graciel Covanzi <41657320+CodeVanzi@users.noreply.github.com>
2024-04-09 23:31:54 -0300

    profile image fixed

--- a_users/forms.py
+++ a_users/forms.py
@@ -1,22 +1,19 @@
 from django import forms
 from a_users.models import CustomProfile
 
-
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = CustomProfile
-        fields = ["image", "nickname", "email", "location", "bio"]
+        exclude = ["user"]
         labels = {
             "image": "Imagem de perfil",
             "nickname": "Apelido",
-            "email": "E-mail",
             "location": "Localização",
             "bio": "Biografia",
         }
         widgets = {
             "image": forms.FileInput(attrs={"class": "form-control"}),
-            "nickname": forms.TextInput(attrs={"class": "form-control"}),
-            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "nickname": forms.TextInput(attrs={"id": "form-nickname", "class": "form-control"}),
             "location": forms.TextInput(attrs={"class": "form-control"}),
             "bio": forms.Textarea(attrs={"class": "form-control"}),
         }
--- a_users/signals.py
+++ a_users/signals.py
@@ -1,26 +1,18 @@
-from django.contrib.auth.models import User  # Importe o modelo de usuário padrão do Django
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from allauth.account.signals import user_signed_up
-from .models import CustomProfile  # Importe o modelo CustomProfile ou substitua pelo seu modelo de perfil de usuário
+from .models import CustomProfile
 
-# Este é um exemplo de como criar um perfil de usuário após a criação do usuário usando sinais do Django Allauth
-
-@receiver(post_save, sender=User)  # Usamos o sinal post_save do Django para o modelo User
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     """
     Função para criar um CustomProfile sempre que um novo usuário é criado.
     """
     if created:
-        CustomProfile.objects.create(user=instance)
+        user = instance
+        email = instance.email  # Acesso ao e-mail do usuário
+        nickname = f'{instance.first_name} {instance.last_name}'  # Criação de um apelido
+        
+        profile = CustomProfile.objects.create(user=user, email=email, nickname=nickname)
 
-@receiver(user_signed_up)  # Usamos o sinal user_signed_up fornecido pelo Django Allauth
-def populate_profile_on_signup(request, user, **kwargs):
-    """
-    Função para popular o perfil do usuário quando o usuário se registra.
-    Aqui você pode definir quaisquer campos adicionais do CustomProfile
-    que deseja preencher com base nos dados fornecidos durante o registro.
-    """
-    # Exemplo: definindo o nome de exibição do usuário no perfil
-    user.profile.display_name = user.username
-    user.profile.save()  # Salve o perfil do usuário
+
