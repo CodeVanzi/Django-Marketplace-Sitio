f74dd041a19f4c9b5dbdd878aa820058c32fa539
Graciel Covanzi <2100688@aluno.univesp.br>
2024-05-17 18:19:31 -0300

    bokeh working - dashboard

--- commodities/views.py
+++ commodities/views.py
@@ -6,32 +6,31 @@ from bokeh.models import ColumnDataSource
 from bokeh.embed import components 
 from bokeh.plotting import figure
 
+
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 @login_required
 def dashboard(request):
-    # max_data = CommoditiesPrices.objects.all().aggregate(Max('data'))
-    # max_preco = CommoditiesPrices.objects.all().aggregate(Max('preco_de_fechamento'))
-    # min_preco = CommoditiesPrices.objects.all().aggregate(Min('preco_de_fechamento'))
+
+    data = {'x_values': [1, 2, 3, 4, 5],
+            'y_values': [6, 7, 2, 3, 6]}
+
+    # create a ColumnDataSource by passing the dict
+    source = ColumnDataSource(data=data)
     
-    count = int(request.GET.get('count', 5))
-    recorte = CommoditiesPrices.objects.all().order_by('data').reverse()[:count]
-    ativo = [r.ativo for r in recorte]
-    preco_ativo = [r.preco_de_fechamento for r in recorte]
+    fig = figure(title="Multiple line example", x_axis_label="x", y_axis_label="y", height=500,)
     
-    cds = ColumnDataSource(data=dict(ativo=ativo, preco_ativo=preco_ativo))
+    # fig.vbar(x='País', top='Numero', width=0.8, source=cds)
+    fig.line(x='x_values', y='y_values', legend_label="Temp.", color="blue", line_width=2, source=source)
+
     
-    fig = figure(x_range=ativo, height=500, title=f' Maiores {{count}} preços de fechamento por ativo',)
-    
-    fig.vbar(x='ativo', top='preco_ativo', width=0.8, source=cds)
-    
-    script, div = components(fig)
+    div, script = components(fig)
 
     context = {
         'script': script,
-        'div': div
+        'div': div,
     }
     
     
