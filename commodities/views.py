541594b39e7abb517d3c1a632846d7eafc87079f
Graciel Covanzi <2100688@aluno.univesp.br>
2024-05-18 15:47:07 -0300

    Update view commodities

--- commodities/views.py
+++ commodities/views.py
@@ -5,33 +5,35 @@ from commodities.models import CommoditiesPrices
 from bokeh.models import ColumnDataSource
 from bokeh.embed import components 
 from bokeh.plotting import figure
-
+from bokeh.layouts import column
 
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
 @login_required
 def dashboard(request):
+    # Dados de exemplo
+    x_values = [1, 2, 3, 4, 5]
+    y_values = [6, 7, 2, 3, 6]
 
-    data = {'x_values': [1, 2, 3, 4, 5],
-            'y_values': [6, 7, 2, 3, 6]}
+    data = {'x_values': x_values, 'y_values': y_values}
 
-    # create a ColumnDataSource by passing the dict
+    # Criar ColumnDataSource
     source = ColumnDataSource(data=data)
-    
-    fig = figure(title="Multiple line example", x_axis_label="x", y_axis_label="y", height=500,)
-    
-    # fig.vbar(x='País', top='Numero', width=0.8, source=cds)
+
+    # Criar figura
+    fig = figure(title="Exemplo de Linha", x_axis_label="x", y_axis_label="y", height=500, width=800)
     fig.line(x='x_values', y='y_values', legend_label="Temp.", color="blue", line_width=2, source=source)
+        
+    # Layout
+    layout = column(fig)
 
-    
-    div, script = components(fig)
+    # Obter os componentes do layout
+    script, div = components(layout)
 
+    # Contexto para o template
     context = {
         'script': script,
         'div': div,
     }
-    
-    
+
     return render(request, 'commodities/dashboard.html', context)
