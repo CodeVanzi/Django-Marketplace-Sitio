e74c0c64626320d775f6cf77a88d9fbe77a9138a
Graciel Covanzi <41657320+CodeVanzi@users.noreply.github.com>
2024-05-19 18:17:17 -0300

    main dashboard bugfix

--- commodities/apps.py
+++ commodities/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CommoditiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'commodities'
+    
+class MediasCommoditiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'medias_commodities'
